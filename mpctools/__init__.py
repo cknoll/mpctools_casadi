19d87909420438538e5ceaf11a1106af24b1cfbc
Michael Risbeck <risbeck@wisc.edu>
2016-02-10 10:54:57 -0600

    Increment version and make sure user's Casadi isn't too new.

--- mpctools/__init__.py
+++ mpctools/__init__.py
@@ -1,19 +1,24 @@
 """
 Import common functions here to make things a bit easier for the users.
 
-Also check the version of Casadi and make sure it is new enough.
+Also check the version of Casadi and make sure it is new enough but not too
+new (due to backward incompatibility in Casadi).
 """
 
 # Version for mpctools.
-__version__ = "2.1"
+__version__ = "2.1.1"
 
 # Check Casadi version to make sure it's up to date.
-MIN_CASADI_VERSION = "2.4"
+_MIN_CASADI_VERSION = "2.4"
+_MAX_CASADI_VERSION = "2.4.3"
 from distutils.version import LooseVersion as getVersion
 import casadi
-if getVersion(casadi.__version__) < getVersion(MIN_CASADI_VERSION):
+if getVersion(casadi.__version__) < getVersion(_MIN_CASADI_VERSION):
     raise ImportError("casadi version %s is too old (must be >=%s)"
-        % (casadi.__version__, MIN_CASADI_VERSION))
+        % (casadi.__version__, _MIN_CASADI_VERSION))
+elif getVersion(casadi.__version__) > getVersion(_MAX_CASADI_VERSION):
+    raise ImportError("casadi version %s is too new (must be <=%s)"
+        % (casadi.__version__, _MAX_CASADI_VERSION))
 
 # Add modules and some specific functions.
 import tools
